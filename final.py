87290840438aaff497547a5b24ad03a584b4451a
Wyattdmiller <Wyatt@MacBook-Pro.routerlogin.net>
2020-12-17 23:09:06 -0800

    pseudo-code

--- final.py
+++ final.py
@@ -8,3 +8,32 @@ Then it will check each grade in the list and compare it to the average.
 It will keep track of how many grades were above the average,
 and display that number as a percentage of the number of grades.
 """
+
+""" Pseudo-code
+def main:
+  file = Final.txt
+  display(file)
+  calculate_percent_above_avg(file)
+
+def display(file):
+  infile = open(file)
+  listGrades = lines of file
+  close file
+  print(number of grades: )
+  print(len(listGrades))
+  avg = sum(listGrades) / len(listGrades)
+  print(Average of grades: )
+  print(avg)
+
+def calculate_percent_above_avg(file):
+  counter = 0
+  for line in file
+    if grade > avg
+      counter +=1
+  percentHigher = counter / len(listGrades)
+  print(The percent of grades above average is: )
+  print(percentHigher %)
+
+main()
+"""
+
