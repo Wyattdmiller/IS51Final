93d974c58200144d7dae88e85d6dcfa5758c6a1c
Wyattdmiller <Wyatt@MacBook-Pro.routerlogin.net>
2020-12-17 23:04:59 -0800

    structured-english

---
+++ final.py
@@ -0,0 +1,10 @@
+""" Structured English
+This program opens the file Final.txt, which is a list of student grades on a final exam. 
+It then creates a list, where each item of the list is a line of the file.
+It displays the number of items in the list, which is the number of grades.
+It then sums the grades in the list, and divides that by the number of grades to get an average.
+It will display the average.
+Then it will check each grade in the list and compare it to the average.
+It will keep track of how many grades were above the average,
+and display that number as a percentage of the number of grades.
+"""
